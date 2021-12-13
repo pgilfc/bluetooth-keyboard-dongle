43380d2870d1db261d7bf77d583654b11f9e537c
gil <pgilfc@gmail.com>
2021-12-13 12:03:44 +0000

    changed bootstrap config

--- bootstrap_keyboard_config.sh
+++ bootstrap_keyboard_config.sh
@@ -2,11 +2,6 @@
 
 cp /etc/rc.local /etc/rc.local.bak
 
-cat<<EOF > /etc/rc.local
-#!/usr/bin/env bash
-sudo bash /bluetooth-keyboard-auto-loader/keyboard_config.sh
-sudo chmod 777 /dev/hidg0
-exit 0
-EOF
+cp rc.local /etc/rc.local
 
 chmod 755 /etc/rc.local
