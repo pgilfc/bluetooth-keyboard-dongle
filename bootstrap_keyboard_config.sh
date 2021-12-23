38772d6bfd5d5643c72801ee2b3481f336b5ad39
gil <pgilfc@gmail.com>
2021-12-23 19:20:56 +0000

    Overall refractoring

--- bootstrap_keyboard_config.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cp /etc/rc.local /etc/rc.local.bak
-
-cp rc.local /etc/rc.local
-
-chmod 755 /etc/rc.local
--- scripts/bootstrap_keyboard_config.sh
+++ scripts/bootstrap_keyboard_config.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
-cp /etc/rc.local /etc/rc.local.bak
-
 cp scripts/rc.local /etc/rc.local
 
 chmod 755 /etc/rc.local
-
-cp /etc/bluetooth/main.conf /etc/bluetooth/main.bak
 
 cp scripts/main.conf /etc/bluetooth/main.conf
 
