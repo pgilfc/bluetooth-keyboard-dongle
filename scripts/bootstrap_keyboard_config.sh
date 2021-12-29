a718e062a00f9d060cd8f1303a5d9ed9a629ea40
gil <pgilfc@gmail.com>
2021-12-29 01:12:05 +0000

    Adapted the report description to my needs

--- scripts/bootstrap_keyboard_config.sh
+++ scripts/bootstrap_keyboard_config.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-cp scripts/rc.local /etc/rc.local
-
-chmod 755 /etc/rc.local
-
 cp scripts/main.conf /etc/bluetooth/main.conf
 
 echo "options btusb enable_autosuspend=n" > /etc/modprobe.d/btusb_disable_autosuspend.conf
--- scripts/keyboard_config.sh
+++ scripts/keyboard_config.sh
@@ -2,40 +2,41 @@
 
 # with help from https://www.rmedgar.com/blog/using-rpi-zero-as-keyboard-setup-and-device-definition
 # and https://github.com/torvalds/linux/blob/master/Documentation/usb/gadget_configfs.rst
+# and http://eleccelerator.com/usbdescreqparser/
 
-# Create gadget
 mkdir /sys/kernel/config/usb_gadget/mykeyboard
 cd /sys/kernel/config/usb_gadget/mykeyboard
 
-# Add basic information
 echo 0x0100 > bcdDevice # Version 1.0.0
 echo 0x0200 > bcdUSB # USB 2.0
 echo 0x00 > bDeviceClass
 echo 0x00 > bDeviceProtocol
 echo 0x00 > bDeviceSubClass
-echo 0x08 > bMaxPacketSize0
+echo 0x16 > bMaxPacketSize0
 echo 0x0104 > idProduct # Multifunction Composite Gadget
 echo 0x1d6b > idVendor # Linux Foundation
 
-# Create English locale
 mkdir strings/0x409
-
 echo "My manufacturer" > strings/0x409/manufacturer
 echo "My virtual keyboard" > strings/0x409/product
 echo "0123456789" > strings/0x409/serialnumber
 
-# Create HID function
 mkdir functions/hid.usb0
-
 echo 1 > functions/hid.usb0/protocol
-echo 8 > functions/hid.usb0/report_length # 8-byte reports
-echo 1 > functions/hid.usb0/subclass
-echo -ne \\x05\\x01\\x09\\x06\\xA1\\x01\\x05\\x07\\x19\\xe0\\x29\\xe7\\x15\\x00\\x25\\x01\\x75\\x01\\x95\\x08\\x81\\x02\\x95\\x01\\x75\\x08\\x81\\x01\\x95\\x03\\x75\\x01\\x05\\x08\\x19\\x01\\x29\\x03\\x91\\x02\\x95\\x05\\x75\\x01\\x91\\x01\\x95\\x06\\x75\\x08\\x15\\x00\\x26\\xff\\x00\\x05\\x07\\x19\\x00\\x2a\\xff\\x00\\x81\\x00\\xc0 > functions/hid.usb0/report_desc
+echo 16 > functions/hid.usb0/report_length
+echo 0 > functions/hid.usb0/subclass
+# cp /sys/class/input/$1/device/device/report_descriptor functions/hid.usb0/report_desc
+# this ^ didnt work for my usecase (keychron k2).
+# it would work just fine in linux, however, in windows it would throw errors
+# what i ended up doing was edit the report descriptor as to leave only the absolute necessary REPORT IDs (in this case 1 and 8)
+# helpful commands:
+# - xxd -i /dev/hidraw0 (translate report descriptor to hex)
+# - evtest (monitor keypresses)
+# - cmp compare binary files
+cp /bluetooth-keyboard-auto-loader/scripts/report_desc functions/hid.usb0/report_desc
 
-# Create configuration
 mkdir configs/c.1
 mkdir configs/c.1/strings/0x409
-
 echo 0x80 > configs/c.1/bmAttributes
 echo 200 > configs/c.1/MaxPower # 200 mA
 echo "Example configuration" > configs/c.1/strings/0x409/configuration
