f085982a4c2261f18b745cfbb5ef294e85b1784c
gil <pgilfc@gmail.com>
2021-12-29 22:30:25 +0000

    Improved bootstrap file

--- scripts/bootstrap_keyboard_config.sh
+++ scripts/bootstrap_keyboard_config.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 
-cp scripts/main.conf /etc/bluetooth/main.conf
+# $1 is suposed to be the root filesystem
+# $2 is suposed to be the /boot filesystem
 
-echo "options btusb enable_autosuspend=n" > /etc/modprobe.d/btusb_disable_autosuspend.conf
+echo "dtoverlay=dwc2" >> $2/config.txt
+
+mkdir $1/bluetooth-keyboard-auto-loader
+
+cp -R . $1/bluetooth-keyboard-auto-loader
+
+cp scripts/main.conf $1/etc/bluetooth/main.conf
+
+echo "options btusb enable_autosuspend=n" > $1/etc/modprobe.d/btusb_disable_autosuspend.conf
+
+cp systemd/bluetooth-keyboard-bypass.service $1/etc/systemd/system/bluetooth-keyboard-bypass.service
+
+cp systemd/bluetooth-keyboard-connect.service $1/etc/systemd/system/bluetooth-keyboard-connect.service
+
+cd $1/etc/systemd/system/multi-user.target.wants/
+
+ln -s bluetooth-keyboard-bypass.service ../bluetooth-keyboard-bypass.service
+
+ln -s bluetooth-keyboard-connect.service ../bluetooth-keyboard-connect.service
